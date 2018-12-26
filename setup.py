754201fe854b8ab95d2b08453a9fecec4376b08a
silane <18604696+silane@users.noreply.github.com>
2018-12-27 03:13:36 +0900

    Make installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='osudb_parser',
+    version='0.0.1',
+    description='Library to parse osu!.db file',
+    author='silane',
+    license='Apache License',
+    packages=find_packages(),
+)
